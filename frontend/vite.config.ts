4d918d571eb5dce98f19cba608a06389a7b5d073
uch2ha <sinyavskiy.d@gmail.com>
2026-09-20 13:24:04 +0300

    dev: add docker-compose.development for locall run with services hot-reload

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -5,6 +6,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   server: {
     host: true,
     port: 5173,
